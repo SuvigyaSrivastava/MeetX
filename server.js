804e33cc6b34b50c5e212a06b05c51d02e46a998
Suvigya <suvigyasrvstva@gmail.com>
2025-05-10 17:07:07 +0530

    Add root route to show API status

--- server.js
+++ server.js
@@ -24,3 +24,8 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+    res.send("MeetX API is running 🚀");
+  });
+  
